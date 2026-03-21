0571548dc20e757f995eb776def31bb6f31edb90
rmecidli26 <rmecidli26@gmail.com>
2026-03-21 16:30:55 +0000

    6 exercise

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i+1,10):
+for i in range(0, 9):
+    for j in range(i+1, 10):
         if i == 0:
-            print("{:02d}".format(j), end=", ")
+            print("{}{}".format(i,j), end=", ")
         elif i != 8:
             print("{}{}".format(i,j),end = ", ")
         else:
