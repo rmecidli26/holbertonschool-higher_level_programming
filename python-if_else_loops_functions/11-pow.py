3b009bda830dbf69c8008144b8e6d671c7084fe8
rmecidli26 <rmecidli26@gmail.com>
2026-03-21 18:12:22 +0000

    11 exercise

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    for i in range(b):
-        result = result * a
+    if b > 0:
+        for i in range(b):
+            result = result * a
+    elif b == 0:
+        result
+    else:
+        for i in range(-b):
+            result = result * (1/a)
     return result
