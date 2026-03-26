c48d45add8c86eb3e148767804ecbe4ca81f1b72
= <=>
2026-03-26 14:26:09 +0000

    2-exese

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     n = len(sys.argv) - 1
     if n == 0:
         print("{} arguments.".format(n))    
-    elif n >= 1:
+    elif n == 1:
         print("{} argument:".format(n))
     else:
         print("{} arguments: ".format(n))
