f568a51e098fceb786134b97d6b135dcb0fce973
= <=>
2026-03-26 14:09:08 +0000

    2-exese

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
-n = len(sys.argv) - 1
 if __name__ == "__main__":
+    n = len(sys.argv) - 1
     if n == 0:
         print("{} arguments.".format(n))    
-    if n >= 1:
-        print("{} argument: ".format(n))
+    elif n >= 1:
+        print("{} argument:".format(n))
         for i in range(1, n+1):
             print("{}: {}".format(i, sys.argv[i]))
