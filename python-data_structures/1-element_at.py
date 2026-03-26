2db2d5a48209cdbf192e0cc8d10bfff3c14ddc0b
rmecidli26 <rmecidli26@gmail.com>
2026-03-26 20:10:17 +0000

    1-exercise

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    return my_list[idx]
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
