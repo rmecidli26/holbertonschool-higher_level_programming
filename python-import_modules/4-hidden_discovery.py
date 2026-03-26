c1a3ac802d9d29d837698f9f9f3e54942b04f570
= <=>
2026-03-26 14:54:19 +0000

    4-exercise

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
+
 if __name__ == "__main__":
     all_names = dir(hidden_4)
     for name in sorted(all_names):
         if not name.startswith("__"):
             print(name)
-            
