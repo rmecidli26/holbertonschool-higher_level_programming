3899d3b0dd8dda68a6905835026655fbb1938b7f
rmecidli26 <rmecidli26@gmail.com>
2026-03-26 19:49:43 +0000

    0-exercise

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def print_list_integer(my_list = []):
     for i in my_list:
         print("{:d}".format(i))
-print_list_integer(my_list)
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
