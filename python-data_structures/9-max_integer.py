bc58de3a37fb3d5c1067bf92f1e1baa810ed9b9f
rmecidli26 <rmecidli26@gmail.com>
2026-03-27 13:16:57 +0000

    9-exeercise

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxi = my_list[0]
-    for i in my_list:
-        if(maxi < i): 
-            maxi = i
-    return maxi
+    if len(my_list) == 0:
+        return "None"
+    else:
+        maxi = my_list[0]
+        for i in my_list:
+            if(maxi < i): 
+                maxi = i
+        return maxi
