fda1d8ce0a384903a22302c2ef2622a4778e50b8
rmecidli26 <rmecidli26@gmail.com>
2026-03-27 12:47:13 +0000

    7-exercise

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    ta = (tuple_a + (0, 0))[:2]
-    tb = (tuple_b + (0, 0))[:2]
-    return (ta[0] + tb[0], ta[1] + tb[1])
-    
+    new_a = (tuple_a + (0, 0))[:2]
+    new_b = (tuple_b + (0, 0))[:2]
+    return (new_a[0] + new_b[0], new_a[1] + new_b[1])
