bbe1e7fecd1a4f7df0fafc97f63e6b741ff723b2
rmecidli26 <rmecidli26@gmail.com>
2026-03-27 11:21:51 +0000

    6-exercise

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print()
+        break
     for i in range(0, len(matrix)):
-        if len(matrix[i]) == 0:
-            print()
             for j in range(0, len(matrix[0])):
                 if j == len(matrix[0]) - 1:
                     print("{:d}".format(matrix[i][j]))
