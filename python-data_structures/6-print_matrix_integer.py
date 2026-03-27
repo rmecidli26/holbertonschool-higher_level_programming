410eff226c18141a790dc105ee723464fa10d9a9
rmecidli26 <rmecidli26@gmail.com>
2026-03-27 11:45:40 +0000

    6-exercise

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not my_list:
+        print()
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[0])):
             if j == len(matrix[0]) - 1:
                 print("{:d}".format(matrix[i][j]))
             else:
                 print("{:d}".format(matrix[i][j]),end=" ")
-        print()
