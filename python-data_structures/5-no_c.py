6dafd85ab2fd1879db7ddbf859a955a4ec1be11b
rmecidli26 <rmecidli26@gmail.com>
2026-03-27 09:13:10 +0000

    5-exercise

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    a = ""
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            a = a + i
+    return a
