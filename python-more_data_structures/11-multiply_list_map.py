de4172341812c7fdeee8f4ae0d1d97e9204e4641
= <=>
2026-03-28 14:09:41 +0000

    11-exercise

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    a = []
-    for i in my_list:
-        a.append(i * number)
-    return a
+    return list(map(lambda x : x * 2 , my_list))
