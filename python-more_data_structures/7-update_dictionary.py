785fbd7b2a6e8f8d997fad1834ebd1a1faf7f782
= <=>
2026-03-28 14:17:55 +0000

    7-exercise

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,8 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
+
+
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for i in sorted_keys:
