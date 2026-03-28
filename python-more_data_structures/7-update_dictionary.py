561866a16a3fc39b9f58fd49d80698c5843491f9
= <=>
2026-03-28 14:16:22 +0000

    7-exercise

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for i in sorted_keys:
+        print("{}: {}".format(i, a_dictionary[i]))
