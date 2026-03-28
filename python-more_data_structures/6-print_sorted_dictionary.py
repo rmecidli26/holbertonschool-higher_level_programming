372478dc86fd7a45587acf4b6e2750266bede2d1
= <rmecidli26@gmail.com>
2026-03-28 09:34:30 +0000

    6-exercise

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for i in sorted_keys:
-        print("{}: {}".format(i,a_dictionary[i]))
+        print("{}: {}".format(i, a_dictionary[i]))
