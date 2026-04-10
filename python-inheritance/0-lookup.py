0fe4cf0930a3f96045d03a4c94eac8d7e87ed489
rmecidli26 <rmecidli26@gmail.com>
2026-04-10 20:06:11 +0000

    0-exercise

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Bu modul obyektlərin atributlarını və metodlarını
+axtarmaq üçün funksiyanı ehtiva edir.
+"""
+
+
 def lookup(obj):
+    """Obyektin mövcud atribut və metodlarının siyahısını qaytarır"""
     return dir(obj)
