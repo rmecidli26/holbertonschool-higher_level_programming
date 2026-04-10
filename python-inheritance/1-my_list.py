bf48f85b51adea30049c0a8f6fc01af080ae021e
rmecidli26 <rmecidli26@gmail.com>
2026-04-10 20:25:05 +0000

    1-exercise

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,8 @@ class MyList(list):
     """Siyahıdan miras alan sinif"""
 
     def print_sorted(self):
-        """Yalnız müsbət tam ədədləri sıralayıb çap edir"""
-        # Siyahıdakı elementlərdən mənfi olmayanları süzgəcdən keçiririk
-        positives = [x for x in self if isinstance(x, int) and x >= 0]
-        print(sorted(positives))
+        """
+        Siyahını artan sıra ilə (mənfi ədədlər daxil) çap edir.
+        Orijinal siyahı dəyişmir.
+        """
+        print(sorted(self))
