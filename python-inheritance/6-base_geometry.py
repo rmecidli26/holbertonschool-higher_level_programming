74d2ee54cf00e603581e9b88d12e82493f83f697
rmecidli26 <rmecidli26@gmail.com>
2026-04-10 20:41:03 +0000

    6-exercise

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A class representing BaseGeometry"""
+
+    def area(self):
+        """Public instance method that raises an Exception"""
+        raise Exception("area() is not implemented")
