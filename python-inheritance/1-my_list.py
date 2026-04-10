99ce81de8ecc76c682934a8d655d61478be0f2fc
rmecidli26 <rmecidli26@gmail.com>
2026-04-10 20:14:32 +0000

    1-exercise

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-class MyList():
+"""MyList sinfi üçün modul"""
 
 
-
+class MyList(list):
+    """Siyahıdan miras alan sinif"""
 
     def print_sorted(self):
+        """Siyahını artan sıra ilə çap edir"""
         print(sorted(self))
