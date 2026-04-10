5c97101016a595e92bc6bb423f2d16cd1419eba9
rmecidli26 <rmecidli26@gmail.com>
2026-04-10 20:32:29 +0000

    2-exercise

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+"""Module for is_same_class"""
 
 
 def is_same_class(obj, a_class):
-    
+    """Function that returns True if object is exactly an instance of a_class"""
     return type(obj) is a_class
