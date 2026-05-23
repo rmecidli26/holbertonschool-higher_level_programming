215df65ff09078c0b6c35b602eb2ae7804c5ea5f
rmecidli26 <rmecidli26@gmail.com>
2026-05-23 08:07:49 +0000

    15-exercise

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT s.title
+FROM tv_shows AS s
+JOIN tv_show_genres AS sg 
+  ON s.id = sg.show_id
+JOIN tv_genres AS g 
+  ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
